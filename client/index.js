72a53d1a0fff957c2cbbc907f76739661349e6c7
Daria Mordvinov <daria.mordvinov@gmail.com>
2022-11-23 13:43:47 -0500

    update

--- client/index.js
+++ client/index.js
@@ -1,9 +1,12 @@
 import React, { useEffect, useState } from 'react';
 import { render } from 'react-dom';
+import { createRoot } from 'react-dom/client';
+import css from "./styles.css";
 
 const App = (props) => {
     const [artists, setArtists] = useState([]);
     const [winner, setWinner] = useState({});
+    const [round, setRound] = useState({played: false, won: false});
 
     useEffect(() => {
         async function fetchData() {
@@ -15,16 +18,53 @@ const App = (props) => {
         fetchData();
     }, []);
 
+    function handleClick(id) {
+        // check if the winning option was clicked
+        if (id === winner.artist) {
+            setRound({played: true, won: true});
+        } else {
+            setRound({played: true, won: false});
+        }
+    }
+
     return (
-        <div>
-          <h1>My super cool app</h1>
-          {artists.map(ar => <p>{ar.name}</p>)}
-          <audio src={winner.trackUrl} controls></audio>
-        </div>
+        <main className='container'>
+            <h1>Spotifyer</h1>
+            <audio src={winner.trackUrl} controls></audio>
+            {round.played && round.won && 
+               <div className='results'>
+                 <h2>Congrats! You guessed right.</h2>
+                 <button onClick={() => {
+                    window.location.reload();
+                }}>Play again</button>
+               </div>
+            }
+            {round.played && !round.won && 
+                <div className='results'>
+                  <h2>You are wrong. This is the song by {winner.name}. It's called {winner.nameOfSong}, dummy</h2>
+                  <button onClick={() => {
+                    window.location.reload();
+                }} >Play again</button>
+                </div>
+            }
+            <div className='artists-container'>
+            {artists.map(ar => 
+                <div className='artist' key={ar.id}>
+                    <div className='img-wrapper'>
+                        <img 
+                          onClick={() => handleClick(ar.id)}
+                          src={ar.image} 
+                        />
+                    </div>
+                    <p>{ar.name}</p>
+                </div>
+            )}
+            </div>
+        </main>
     )
 }
 
-render(
-    <App />,
-    document.querySelector('#root')
+createRoot(document.querySelector('#root'))
+  .render(
+    <App />
 )
--- server/server.js
+++ server/server.js
@@ -9,6 +9,7 @@ const PORT = 8080;
 
 app.use(express.json());
 app.use(bodyParser.json());
+app.use(express.static('client'))
 
 app.use('/', 
   gameController.getArtistId, 
