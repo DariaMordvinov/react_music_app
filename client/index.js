554de4028883450e4673994af73484495818e455
Daria Mordvinov <daria.mordvinov@gmail.com>
2022-11-23 09:08:03 -0500

    backend done

--- client/index.js
+++ client/index.js
@@ -2,13 +2,15 @@ import React, { useEffect, useState } from 'react';
 import { render } from 'react-dom';
 
 const App = (props) => {
-    const [token, setToken] = useState('');
+    const [artists, setArtists] = useState([]);
+    const [winner, setWinner] = useState({});
 
     useEffect(() => {
         async function fetchData() {
             const response = await fetch('http://localhost:8080/');
-            const token = await response.json();
-            setToken(token);
+            const {artists, winner} = await response.json();
+            setArtists(artists);
+            setWinner(winner);
         }
         fetchData();
     }, []);
@@ -16,7 +18,8 @@ const App = (props) => {
     return (
         <div>
           <h1>My super cool app</h1>
-          {token}
+          {artists.map(ar => <p>{ar.name}</p>)}
+          <audio src={winner.trackUrl} controls></audio>
         </div>
     )
 }
--- server/server.js
+++ server/server.js
@@ -10,9 +10,12 @@ const PORT = 8080;
 app.use(express.json());
 app.use(bodyParser.json());
 
-app.use('/', gameController.getOptions, (req, res) => {
+app.use('/', 
+  gameController.getArtistId, 
+  gameController.getArtistOptions, 
+  gameController.getTrack, (req, res) => {
   // return res.status(200).sendFile(path.resolve(__dirname, '../index.html'));
-  return res.setHeader('Access-Control-Allow-Origin', '*').status(200).json(res.locals.token);
+  return res.setHeader('Access-Control-Allow-Origin', '*').status(200).json({artists: res.locals.arr, winner: res.locals.winner});
 })
 
 // check if we're listening on the right port
